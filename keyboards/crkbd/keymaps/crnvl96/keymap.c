424d832c3ad755beb176b29c5688aeed8e5ecb99
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2025-01-25 12:12:11 -0300

    feat(crkbd): combos

--- keyboards/crkbd/keymaps/crnvl96/keymap.c
+++ keyboards/crkbd/keymaps/crnvl96/keymap.c
@@ -26,6 +26,28 @@ enum layer {
     _U_FUN,
 };
 
+enum combos {
+    WE,
+    ER,
+    YU,
+    UI,
+    IO,
+};
+
+const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM yu_combo[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+
+combo_t key_combos[] = {
+    [WE] = COMBO(we_combo, KC_ESC),
+    [ER] = COMBO(er_combo, KC_ENT),
+    [YU] = COMBO(yu_combo, KC_DEL),
+    [UI] = COMBO(ui_combo, KC_TAB),
+    [IO] = COMBO(io_combo, KC_CAPS),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_U_BASE] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
